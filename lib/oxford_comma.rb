8be32e1631c213efa0cc7da8ab1e4e6039391bcd
Nina Angelo <ninacelie@gmail.com>
2019-12-11 15:11:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,10 @@ def oxford_comma(array)
   array.join ("")
 
 elsif array.length == 2
-  array.join ( " and " )
+  array.join (" and ")
 elsif array.length >= 3
   array[-1] = "and #{array[-1]}"
+  array.join (" , ")
 end
 
 
